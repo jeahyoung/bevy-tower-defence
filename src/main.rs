331b186bb33d6f637d99265a235c842ea8093b49
lj <jeahyoung@gmail.com>
2023-05-10 00:08:20 +0900

    camera controls

--- src/main.rs
+++ src/main.rs
@@ -41,7 +41,46 @@ fn main() {
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_basic_scene)
         .add_startup_system(asset_loading)
+        .add_system(camera_controls)
         .run();
+}
+
+fn camera_controls(
+    keyboard: Res<Input<KeyCode>>,
+    mut camera_query: Query<&mut Transform, With<Camera3d>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut camera) = camera_query.get_single_mut() {
+        let mut forward = camera.forward();
+        forward.y = 0.0;
+        forward = forward.normalize();
+
+        let mut left = camera.left();
+        left.y = 0.0;
+        left = left.normalize();
+
+        let speed = 3.0;
+        let rotate_speed = 0.3;
+
+        if keyboard.pressed(KeyCode::W) {
+            camera.translation += forward * time.delta_seconds() * speed;
+        }
+        if keyboard.pressed(KeyCode::S) {
+            camera.translation -= forward * time.delta_seconds() * speed;
+        }
+        if keyboard.pressed(KeyCode::A) {
+            camera.translation += left * time.delta_seconds() * speed;
+        }
+        if keyboard.pressed(KeyCode::D) {
+            camera.translation -= left * time.delta_seconds() * speed;
+        }
+        if keyboard.pressed(KeyCode::Q) {
+            camera.rotate_axis(Vec3::Y, rotate_speed * time.delta_seconds())
+        }
+        if keyboard.pressed(KeyCode::E) {
+            camera.rotate_axis(Vec3::Y, -rotate_speed * time.delta_seconds())
+        }
+    }
 }
 
 fn asset_loading(mut commands: Commands, assets: Res<AssetServer>) {
